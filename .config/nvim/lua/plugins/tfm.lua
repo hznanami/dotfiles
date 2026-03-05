b55228873ca1c429d9d5ead81f610871717a6b42
hznanami <hznanami770@gmail.com>
2026-03-05 23:58:49 +0800

    chore(nvim/plugins/tfm.nvim): comment out the key bindings in the example configuration

--- .config/nvim/lua/plugins/tfm.lua
+++ .config/nvim/lua/plugins/tfm.lua
@@ -17,14 +17,14 @@ return {
         enable_cmds = true, 
         -- Custom keybindings only applied within the TFM buffer
         -- Default: {}
-        keybindings = {
-            ["<ESC>"] = "q",
-            -- Override the open mode (i.e. vertical/horizontal split, new tab)
-            -- Tip: you can add an extra `<CR>` to the end of these to immediately open the selected file(s) (assuming the TFM uses `enter` to finalise selection)
-            ["<C-v>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.vsplit)<CR>",
-            ["<C-x>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.split)<CR>",
-            ["<C-t>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.tabedit)<CR>",
-        },
+        -- keybindings = {
+        --     ["<ESC>"] = "q",
+        --     -- Override the open mode (i.e. vertical/horizontal split, new tab)
+        --     -- Tip: you can add an extra `<CR>` to the end of these to immediately open the selected file(s) (assuming the TFM uses `enter` to finalise selection)
+        --     ["<C-v>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.vsplit)<CR>",
+        --     ["<C-x>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.split)<CR>",
+        --     ["<C-t>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.tabedit)<CR>",
+        -- },
         -- Customise UI. The below options are the default
         ui = {
             border = "rounded",
