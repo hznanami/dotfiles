d02e545889a86098a2d6ecc945897142f6181793
hznanami <hznanami770@gmail.com>
2026-08-28 03:06:21 +0800

    nvim/plugins: disable pywal16 plugin

--- .config/nvim/lua/plugins/pywal16.lua
+++ .config/nvim/lua/plugins/pywal16.lua
@@ -2,6 +2,7 @@ return {
   'uZer/pywal16.nvim',
   -- for local dev replace with:
   -- dir = '~/your/path/pywal16.nvim',
+  cond = false,
   config = function()
     vim.cmd.colorscheme("pywal16")
   end,
